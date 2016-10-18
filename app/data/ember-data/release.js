9798ee9fa2c292a2df202c856b544ba98b032cf1
bmac <bmac325@gmail.com>
2016-10-18 09:43:07 -0400

    Update Ember Data for the 2.9.0 and 2.10.0-beta.1 release

--- app/data/ember-data/release.js
+++ app/data/ember-data/release.js
@@ -3,10 +3,10 @@ export default {
   baseFileName: 'ember-data',
   projectFilter: [ /ember-data\./ ],
   projectRepo: 'emberjs/data',
-  lastRelease: "2.8.0",
-  futureVersion: "2.8.1",
+  lastRelease: "2.9.0",
+  futureVersion: "2.9.1",
   channel: "release",
-  date: "2016-09-10",
+  date: "2016-10-18",
   changelogPath: "CHANGELOG.md",
   debugFileName: ".js",
   installWithEmberCLI: '# Install Ember-Data %s:\nnpm install --save-dev ember-data@%s\n# Or, install the latest build of this channel which may include unreleased incremental changes:\nnpm install --save-dev emberjs/data#release'
