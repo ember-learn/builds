9aa020899e6ebef9c45b1e81ba48ac5cd8d430b4
Ricardo Mendes <rokusu@gmail.com>
2016-12-06 10:57:13 +0000

    Fixes tagged releases

--- app/routes/tagged.js
+++ app/routes/tagged.js
@@ -4,9 +4,9 @@ import S3Bucket from 'builds/lib/s3-bucket';
 export default Ember.Route.extend({
   model() {
     return S3Bucket.create({
-        title: 'Tagged Builds',
-        prefix: 'tagged/',
-        delimiter: ''
+      title: 'Tagged Release Builds',
+      prefix: 'tags/v',
+      delimiter: ''
     });
   }
 });
