087fccd7cb1ebb0819d77cb67963af262fbe5aef
Robert Jackson <rjackson@linkedin.com>
2017-01-23 23:54:41 -0500

    Update data for 2.11.0 and 2.12.0-beta.1 releases.

--- app/data/ember-data/release.js
+++ app/data/ember-data/release.js
@@ -3,10 +3,10 @@ export default {
   baseFileName: 'ember-data',
   projectFilter: [ /ember-data\./ ],
   projectRepo: 'emberjs/data',
-  lastRelease: "2.10.0",
-  futureVersion: "2.10.1",
+  lastRelease: "2.11.0",
+  futureVersion: "2.11.1",
   channel: "release",
-  date: "2016-11-28",
+  date: "2017-01-09",
   changelogPath: "CHANGELOG.md",
   debugFileName: ".js",
   installWithEmberCLI: '# Install Ember-Data %s:\nnpm install --save-dev ember-data@%s\n# Or, install the latest build of this channel which may include unreleased incremental changes:\nnpm install --save-dev emberjs/data#release'
