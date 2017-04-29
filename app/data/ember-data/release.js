6b71b8468574a93c7a44638c862a0239f383caea
Robert Jackson <rjackson@linkedin.com>
2017-04-29 08:35:08 -0400

    Update ember-data for 2.13 and 2.14.

--- app/data/ember-data/release.js
+++ app/data/ember-data/release.js
@@ -3,10 +3,10 @@ export default {
   baseFileName: 'ember-data',
   projectFilter: [ /ember-data\./ ],
   projectRepo: 'emberjs/data',
-  lastRelease: "2.12.0",
-  futureVersion: "2.12.1",
+  lastRelease: "2.13.0",
+  futureVersion: "2.13.1",
   channel: "release",
-  date: "2017-03-13",
+  date: "2017-04-28",
   changelogPath: "CHANGELOG.md",
   debugFileName: ".js",
   installWithEmberCLI: '# Install Ember-Data %s:\nnpm install --save-dev ember-data@%s\n# Or, install the latest build of this channel which may include unreleased incremental changes:\nnpm install --save-dev emberjs/data#release'
