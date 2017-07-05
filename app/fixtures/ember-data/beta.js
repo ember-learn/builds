6e179c7bf4fdd224952c0e4af3d0b9c5922228db
Ricardo Mendes <rokusu@gmail.com>
2017-07-05 18:26:26 +0100

    Update beta.js

--- app/fixtures/ember-data/beta.js
+++ app/fixtures/ember-data/beta.js
@@ -7,7 +7,7 @@ export default {
   futureVersion: "2.15.0-beta.2",
   finalVersion: '2.15.0',
   channel: "beta",
-  date: "2017-07-18",
+  date: "2017-06-18",
   changelogPath: "CHANGELOG.md",
   debugFileName: ".js",
   installWithEmberCLI: '# Install Ember-Data %s:\nnpm install --save-dev ember-data@%s\n# Or, install the latest build of this channel which may include unreleased incremental changes:\nnpm install --save-dev emberjs/data#beta'
