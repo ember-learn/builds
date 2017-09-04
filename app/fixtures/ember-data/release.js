2f4cd092c3b89fa503b0eafa100e7f4c9c0b7785
Ricardo Mendes <rokusu@gmail.com>
2017-09-04 15:36:19 +0100

    Ember Data v2.15.0

--- app/fixtures/ember-data/release.js
+++ app/fixtures/ember-data/release.js
@@ -3,10 +3,10 @@ export default {
   baseFileName: 'ember-data',
   projectFilter: [ /ember-data\./ ],
   projectRepo: 'emberjs/data',
-  lastRelease: "2.14.3",
-  futureVersion: "2.14.4",
+  lastRelease: "2.15.0",
+  futureVersion: "2.15.1",
   channel: "release",
-  date: "2017-06-22",
+  date: "2017-09-01",
   changelogPath: "CHANGELOG.md",
   debugFileName: ".js",
   installWithEmberCLI: '# Install Ember-Data %s:\nnpm install --save-dev ember-data@%s\n# Or, install the latest build of this channel which may include unreleased incremental changes:\nnpm install --save-dev emberjs/data#release'
