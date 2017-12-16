1b31944b518c1923d052841f914ddf08d19a6829
Onezino Gabriel <zinogabriel@gmail.com>
2017-12-16 09:36:09 -0300

    Update project  beta, lts and release versions (#55)
    * Update project  beta, lts and release versions
    
    * update ember-data versions

--- app/fixtures/ember-data/release.js
+++ app/fixtures/ember-data/release.js
@@ -3,10 +3,10 @@ export default {
   baseFileName: 'ember-data',
   projectFilter: [ /ember-data\./ ],
   projectRepo: 'emberjs/data',
-  lastRelease: "2.16.3",
-  futureVersion: "2.16.4",
+  lastRelease: "2.17.0",
+  futureVersion: "2.17.1",
   channel: "release",
-  date: "2017-10-12",
+  date: "2017-11-19",
   changelogPath: "CHANGELOG.md",
   debugFileName: ".js",
   installWithEmberCLI: '# Install Ember-Data %s:\nnpm install --save-dev ember-data@%s\n# Or, install the latest build of this channel which may include unreleased incremental changes:\nnpm install --save-dev emberjs/data#release'
