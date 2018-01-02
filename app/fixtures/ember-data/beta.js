a455c54e3776e4495d86a774386752c482265946
Ricardo Mendes <rokusu@gmail.com>
2018-01-02 08:50:10 +0000

    Update with 2.18 releases (#57)

--- app/fixtures/ember-data/beta.js
+++ app/fixtures/ember-data/beta.js
@@ -1,13 +1,13 @@
 export default {
   projectName: "Ember Data",
   baseFileName: 'ember-data',
-  projectFilter: [ /ember-data\./ ],
+  projectFilter: [/ember-data\./],
   projectRepo: 'emberjs/data',
-  lastRelease: "2.18.0-beta.1",
-  futureVersion: "2.18.0-beta.2",
-  finalVersion: '2.18.0',
+  lastRelease: "3.0.0-beta.1",
+  futureVersion: "3.0.0-beta.2",
+  finalVersion: '3.0.0',
   channel: "beta",
-  date: "2017-11-19",
+  date: "2017-12-30",
   changelogPath: "CHANGELOG.md",
   debugFileName: ".js",
   installWithEmberCLI: '# Install Ember-Data %s:\nnpm install --save-dev ember-data@%s\n# Or, install the latest build of this channel which may include unreleased incremental changes:\nnpm install --save-dev emberjs/data#beta'
