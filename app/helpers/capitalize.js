3b5e813a2e6eb9f8a826292bd892221f544fa33b
Robert Jackson <rjackson@linkedin.com>
2018-01-14 12:51:45 -0500

    Fix linting failures after eslint config updates.

--- app/helpers/capitalize.js
+++ app/helpers/capitalize.js
@@ -1,8 +1,8 @@
-import Ember from 'ember';
+import { helper } from '@ember/component/helper';
 import { capitalize as capitalizeUtil } from '@ember/string';
 
 export function capitalize([name]/*, hash*/) {
   return capitalizeUtil(name);
 }
 
-export default Ember.Helper.helper(capitalize);
+export default helper(capitalize);
