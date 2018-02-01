6e7ed6dee032b41e3773248958eae82d4a71e810
Ricardo Mendes <rokusu@gmail.com>
2018-02-01 02:34:06 +0000

    Tweaks language away from builds

--- app/controllers/canary.js
+++ app/controllers/canary.js
@@ -3,5 +3,5 @@ import ProjectsMixin from '../mixins/projects';
 
 export default Controller.extend(ProjectsMixin, {
   channel: 'canary',
-  channelDescription: 'Canary builds are generated from each commit to the master branch of Ember and Ember-Data. These builds are not suitable for use in production, and may contain unstable features disabled behind a flag.'
+  channelDescription: 'Canary releases are generated from each commit to the master branch of Ember and Ember Data. These builds are not suitable for use in production, and may contain unstable features disabled behind a flag.'
 });
--- app/controllers/release.js
+++ app/controllers/release.js
@@ -4,5 +4,5 @@ import ProjectsMixin from '../mixins/projects';
 export default Controller.extend(ProjectsMixin, {
   channel: 'release',
   includeReleasesInFileList: true,
-  channelDescription: 'Release builds are production-ready versions of Ember and Ember-Data that have been through a six-week beta cycle.'
+  channelDescription: 'Releases are production-ready versions of Ember and Ember Data that have been through a six-week beta cycle.'
 });
