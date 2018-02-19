780092be90aad1c993004f3927bc16c6e5e90970
Robert Wagner <rwwagner90@gmail.com>
2018-02-18 20:38:33 -0500

    Remove sass and link-to actives

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -6,8 +6,24 @@ export default Controller.extend({
     return this.isActiveChannel('index');
   }),
 
+  isTaggedActive: computed('currentRouteName', function() {
+    return this.isActiveChannel('tagged');
+  }),
+
   isChannelsActive: computed('currentRouteName', function() {
     return !['index','tagged'].some(name => name === this.currentRouteName);
+  }),
+
+  isReleaseActive: computed('currentRouteName', function() {
+    return this.isActiveChannel('release');
+  }),
+
+  isBetaActive: computed('currentRouteName', function() {
+    return this.isActiveChannel('beta');
+  }),
+
+  isCanaryActive: computed('currentRouteName', function() {
+    return this.isActiveChannel('canary');
   }),
 
   isActiveChannel(channel) {
