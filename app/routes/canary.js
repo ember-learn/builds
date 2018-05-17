8a97f3382650b1000159cee651a2199025c95214
Ricardo Mendes <rokusu@gmail.com>
2018-05-17 15:16:07 +0100

    Move setupController logic into computed property

--- app/routes/canary.js
+++ app/routes/canary.js
@@ -1,17 +1,12 @@
-import Route from '@ember/routing/route';
-import fetch from 'fetch';
+import Route from "@ember/routing/route";
+import fetch from "fetch";
 
 export default Route.extend({
   model() {
-    return fetch('https://s3.amazonaws.com/builds.emberjs.com/canary.json').then((response) => {
+    return fetch(
+      "https://s3.amazonaws.com/builds.emberjs.com/canary.json"
+    ).then(response => {
       return response.json();
     });
-  },
-
-  setupController(controller, model){
-    const installWithEmberCLI = `# Install the latest Ember canary:
-npm install --save-dev https://s3.amazonaws.com/builds.emberjs.com${model.assetPath}`;
-
-    controller.set('installWithEmberCLI', installWithEmberCLI)
   }
 });
